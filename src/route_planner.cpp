a93dd486680333dbd9de187c4a492f7eeb85be0b
MomenAliSwiftAct <92243778+MomenAliSwiftAct@users.noreply.github.com>
2024-01-08 10:41:34 +0200

    BUG at Route Planner
    Solved

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -14,7 +14,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     // Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
     RoutePlanner::start_node = &m_Model.FindClosestNode(start_x,start_y);
-    RoutePlanner::end_node = &m_Model.FindClosestNode(start_x,start_y);
+    RoutePlanner::end_node = &m_Model.FindClosestNode(end_x,end_y);
 }
 
 
